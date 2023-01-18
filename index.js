7ccea68a6528ba0104f25b7faaf6934345f55223
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-01-18 08:03:32 +0000

    add dep and start server

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-let a=5
-let b=10
+import express from 'express';
+import jwt from 'jsonwebtoken'
 
-console.log(a+b, 'hello')
+const app=express()
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('Hello Client')
+})
+
+app.post('/auth/login',(req,res)=>{
+    console.log(req.body)
+
+    const token = jwt.sign({
+        email:req.body.email,
+       
+    },'secret123')
+    res.json({success:true,token})
+})
+
+app.listen(4444,(err)=>{
+    if(err){
+        return console.log(err)
+    }
+    console.log('Server OK')
+})

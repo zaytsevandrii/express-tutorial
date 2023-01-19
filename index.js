6811a83ef6590c5810b4cbe29f7520e708854171
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-01-19 09:39:18 +0000

    add dep for upload images and create routes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import { registerValidator,loginValidation, postCreateValidation} from './valida
 import checkAuth from './utils/checkAuth.js'
 import { register,login,getMe } from './controllers/UserController.js';
 import * as PostController from './controllers/PostController.js';
+import multer from 'multer';
+
 
 mongoose.set('strictQuery', true)
 mongoose.connect('mongodb+srv://andrii:04071990@cluster0.bpsvecv.mongodb.net/users?retryWrites=true&w=majority').then(()=>{
@@ -12,14 +14,32 @@ mongoose.connect('mongodb+srv://andrii:04071990@cluster0.bpsvecv.mongodb.net/use
 
 const app=express()
 
+const storage = multer.diskStorage({
+    destination:(_,__,cb)=>{
+        cb(null,'uploads')
+    },
+    filename:(_,file,cb)=>{
+        cb(null,file.originalname)
+    },
+})
+
+const upload = multer({storage})
+
 app.use(express.json())
 
 app.post('/auth/login', login)
 app.post('/auth/register',registerValidator,register)
 app.get('/auth/me',checkAuth,getMe)
 
-app.get('/posts',checkAuth,PostController.getAll)
-app.get('/posts/:id',checkAuth,PostController.getOne)
+app.post('/uploads',checkAuth,upload.single('image'),(req,res)=>{
+    res.json({
+        url:`/uploads/${req.file.originalname}`,
+    })
+})
+app.use('/uploads',express.static('uploads'))
+
+app.get('/posts',PostController.getAll)
+app.get('/posts/:id',PostController.getOne)
 app.post('/posts',checkAuth,postCreateValidation,PostController.create)
 app.delete('/posts/:id',checkAuth,PostController.remove)
 app.patch('/posts/:id',checkAuth,PostController.update)
